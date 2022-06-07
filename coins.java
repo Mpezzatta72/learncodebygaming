d0821c6633cfea3cc8352983c7dfa97de5256ea1
Mpezzatta72 <103334220+Mpezzatta72@users.noreply.github.com>
2022-06-07 15:45:47 -0500

    ArrayList is where the problem is I think?

--- coins.java
+++ coins.java
@@ -1,8 +1,12 @@
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class coins {
 
     public void draw(Graphics g, Board board) {
+    }
+
+    public static void removeAll(ArrayList collectedCoins) {
     }
 
 }
